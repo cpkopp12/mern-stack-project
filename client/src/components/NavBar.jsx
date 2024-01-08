bfccdd9543e3f1aabb0d7fd8df9759ebe0f9e55d
cpkopp <cameronpkopp@gmail.com>
2024-01-08 13:23:23 -1000

    dashboard structure

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import Wrapper from '../assets/wrappers/Navbar';
+
+const NavBar = () => {
+  return (
+    <Wrapper>
+      <div>NavBar</div>
+    </Wrapper>
+  );
+};
+export default NavBar;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Logo } from './Logo.jsx';
 export { default as FormRow } from './FormRow.jsx';
+export { default as BigSidebar } from './BigSidebar.jsx';
+export { default as NavBar } from './NavBar.jsx';
+export { default as SmallSidebar } from './SmallSidebar.jsx';
