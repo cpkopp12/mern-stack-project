a5bb761d34fabd8d67da9ad83bb8673a02ff838f
cpkopp <cameronpkopp@gmail.com>
2024-01-11 17:53:23 -1000

    crud operations jobs api

--- server.js
+++ server.js
@@ -59,6 +59,43 @@ app.post('/api/v1/jobs', (req, res) => {
   res.status(201).json({ job });
 });
 
+// EDIT JOB: /api/v1/jobs/:id
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+
+  // check for missing in body
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'Please provide company and position' });
+  }
+
+  const job = jobs.find((job) => job.id === id);
+  // check for wrong job id
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+});
+
+// DELETE JOB BY ID: /api/v1/jobs/:id
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: 'job deleted' });
+});
+
+// SET PORT AND LISTEN -----------------------
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
