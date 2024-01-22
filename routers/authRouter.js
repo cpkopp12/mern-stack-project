2f859b938890648b86039b0251afbef5d54621f7
cpkopp <cameronpkopp@gmail.com>
2024-01-22 12:06:02 -1000

    logout user route

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,6 +1,6 @@
 // IMPORTS ---------------
 import { Router } from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login, logout } from '../controllers/authController.js';
 import {
   validateRegisterInput,
   validateLoginInput,
@@ -11,6 +11,7 @@ const router = Router();
 
 router.post('/register', validateRegisterInput, register);
 router.post('/login', validateLoginInput, login);
+router.get('/logout', logout);
 
 // EXPORT ROUTER ---------------------------
 export default router;
