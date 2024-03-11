fb0215e05be24b5ce827884e70248681a9a26957
cpkopp12 <cameronpkopp@gmail.com>
2024-03-10 21:38:41 -0400

    public folder

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 // IMPORTS ---------------------------
 // packages
-import 'express-async-errors';
-import * as dotenv from 'dotenv';
+import "express-async-errors";
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
+import express from "express";
 const app = express();
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // local files
-import jobRouter from './routers/jobRouter.js';
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import authRouter from './routers/authRouter.js';
-import { authenticateUser } from './middleware/authMiddleware.js';
-import userRouter from './routers/userRouter.js';
+import jobRouter from "./routers/jobRouter.js";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import authRouter from "./routers/authRouter.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
+import userRouter from "./routers/userRouter.js";
 
 // APP SETUP ---------------------------------------------------
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
+
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api/v1/jobs', authenticateUser, jobRouter);
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users/', authenticateUser, userRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users/", authenticateUser, userRouter);
 
 // test proxy
 // app.get('/api/v1/test', (req, res) => {
@@ -34,8 +41,8 @@ app.use('/api/v1/users/', authenticateUser, userRouter);
 // });
 
 // 404 catch all
-app.use('*', (req, res) => {
-  res.status(404).json({ mgs: 'not found' });
+app.use("*", (req, res) => {
+  res.status(404).json({ mgs: "not found" });
 });
 
 app.use(errorHandlerMiddleware);
