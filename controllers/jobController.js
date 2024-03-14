4c8c209d9540d67554864e04f6742e282873d109
cpkopp12 <cameronpkopp@gmail.com>
2024-03-14 18:01:35 -0400

    stats backend, mongo pipeline

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,6 +2,8 @@
 import { StatusCodes } from "http-status-codes";
 // job model
 import Job from "../models/JobModel.js";
+import mongoose from "mongoose";
+import day from "dayjs";
 
 // EXPORT FUNCTIONS ----------------------------
 // get all jobs
@@ -40,4 +42,53 @@ export const deleteJobById = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(id);
 
   res.status(StatusCodes.OK).json({ job: removedJob });
+};
+
+// show stats
+export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$jobStatus", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format("MMM YY");
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
--- routers/jobRouter.js
+++ routers/jobRouter.js
@@ -6,6 +6,7 @@ import {
   createJob,
   editJobById,
   deleteJobById,
+  showStats,
 } from "../controllers/jobController.js";
 import {
   validateJobInput,
@@ -20,6 +21,9 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router
   .route("/:id")
   .get(validateIdParam, getJobById)
